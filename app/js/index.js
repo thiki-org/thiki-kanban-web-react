ed3a203f6ebf59a0c8b93b9b3ab1d13fd42c9d8c
zengzhu <zengzhu@hoolai.com>
2016-05-30 19:02:30 +0800

    render static html via a react component

--- app/js/index.js
+++ app/js/index.js
@@ -3,22 +3,42 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ExampleApplication = React.createClass({
+var Entrys = React.createClass({
   render: function() {
     var elapsed = Math.round(this.props.elapsed  / 100);
     var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
     var message =
       'React has been successfully running for ' + seconds + ' seconds.';
 
-    return <p>{message}</p>;
+    return (
+        <div className="entry">
+            <div id="entry1001" className="entry-item entryTitle">
+                entry's title
+            </div>
+            <div className="tasks">
+                <div className="task">
+                    <span className="task-title">task's.summary</span>
+                </div>
+            </div>
+            <div className="tasks">
+                <div className="task">
+                    <span className="task-title">task's.summary</span>
+                </div>
+            </div>
+            <div className="tasks">
+                <div className="task">
+                    <span className="task-title">task's.summary</span>
+                </div>
+            </div>
+        </div>
+    );
   }
 });
 
-var start = new Date().getTime();
+var ec = document.getElementById('entry-container');
+console.log('ec='+ec);
 
-setInterval(function() {
-  ReactDOM.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
-  );
-}, 50);
+ReactDOM.render(
+    <Entrys/>,
+    ec
+);

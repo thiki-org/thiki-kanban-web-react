f02f3dab4e14fadf6c7e715332984db886bdceb1
zengzhu <zengzhu@hoolai.com>
2016-05-30 20:59:03 +0800

    using task component with key

--- app/js/index.js
+++ app/js/index.js
@@ -3,34 +3,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Entrys = React.createClass({
-  render: function() {
-      var tasks = [];
-      for (var i = 0; i < 4; i++) {
-          tasks.push(
-              <div className="task">
-                  <span className="task-title">task[{i}]'s.summary</span>
-              </div>
-          );
-      }
 
-      return (
-          <div className="entry">
-              <div id="entry1001" className="entry-item entryTitle">
-                  entry's title
-              </div>
-              <div className="tasks">
-                  {tasks}
-              </div>
-          </div>
-      );
-  }
+var Task = React.createClass({
+    render: function() {
+        return (
+            <div className="task">
+                <span className="task-title">{this.props.data.summary}</span>
+            </div>
+        );
+    }
+
 });
 
-var ec = document.getElementById('entry-container');
-console.log('ec='+ec);
+var Entrys = React.createClass({
+    render: function() {
+        var tasks = this.props.tasks;
+        return (
+            <div className="entry">
+                <div id="entry1001" className="entry-item entryTitle">
+                    entry's title
+                </div>
+                <div className="tasks">
+                    {
+                        tasks.map(function (task){
+                            return <Task key={task.id} data={task} />
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+});
+
+var destination = document.getElementById('entry-container');
+var tasks = [
+    {
+        "id": "task10002",
+        "summary": "swimming 1200 meters"
+    },
+    {
+        "id": "task10005",
+        "summary": "run 10000 meters"
+    }
+];
 
 ReactDOM.render(
-    <Entrys/>,
-    ec
+    <Entrys tasks={tasks}/>,
+    destination
 );
